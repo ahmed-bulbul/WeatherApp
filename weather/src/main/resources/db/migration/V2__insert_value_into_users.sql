b8d67b9c692278640dd3c8a1a4fd71dc4b9b54d1
Mustafa <gulam@monetizenow.io>
2023-07-25 21:19:30 +0600

    update password

--- weather/src/main/resources/db/migration/V2__insert_value_into_users.sql
+++ weather/src/main/resources/db/migration/V2__insert_value_into_users.sql
@@ -1,11 +1,12 @@
+-- pwd:demoUser
 INSERT INTO users
 (id,created_by, creation_date, last_modified_by, last_modified_date, email, "password", username)
-VALUES(1, 1, NOW(), 1, NOW(),'user@gmail.com', '$2a$10$lg6nhtbEYilsNh2SjMMkZuWIxhju0jOhdBexlFxHiVl0ZUljEzSOG', 'user');
+VALUES(1, 1, NOW(), 1, NOW(),'user@gmail.com', '$2a$12$gZA5L01coFBboejZjqPpnuOQOlnoa7G/CsqFGr3EbdqL2bQVuBMFm', 'user');
 
-
+-- pwd:demoAdmin
 INSERT INTO users
 (id,created_by, creation_date, last_modified_by, last_modified_date, email, "password", username)
-VALUES (2, 1, NOW(), 1, NOW(), 'admin@gmail.com', '$2a$10$0sc6BRSFfixENhIK6BPciOKJv0SXF/TPioQTInZjIHF8A6DzjOqKe', 'admin');
+VALUES (2, 1, NOW(), 1, NOW(), 'admin@gmail.com', '$2a$12$XIHNjtPd8vMlq5OIHlDHReDt9lxrwqPAG1UAgCB70puys92.64/7a', 'admin');
 
 INSERT INTO roles
 (id,created_by, creation_date, last_modified_by, last_modified_date, "name")
